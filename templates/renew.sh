0094eebcd578be0abb4efceb4c8825722e87ad2f
Karl Glatz <karl@glatz.biz>
2018-02-05 15:47:00 +0100

    add renew hook support

--- templates/renew.sh
+++ templates/renew.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-certbot renew {% if certbot_pre_hook is defined %}--pre-hook '{{ certbot_pre_hook }}'{% endif %} {% if certbot_post_hook is defined %}--post-hook '{{ certbot_post_hook }}'{% endif %}>&1
+certbot renew {% if certbot_pre_hook is defined %}--pre-hook '{{ certbot_pre_hook }}'{% endif %} {% if certbot_post_hook is defined %}--post-hook '{{ certbot_post_hook }}'{% endif %} {% if certbot_renew_hook is defined %}--renew-hook '{{ certbot_renew_hook }}'{% endif %}>&1
